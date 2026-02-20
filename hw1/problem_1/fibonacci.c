c2b0fefee345b37aa913a9e212b53498c6cd9359
brenux <brendan-brooks@uiowa.edu>
2026-02-20 15:58:55 -0600

    check that input n>=1

--- hw1/problem_1/fibonacci.c
+++ hw1/problem_1/fibonacci.c
@@ -3,7 +3,13 @@
 
 int main(int argc, char* argv[]) {
 
-    const int n = atoi(argv[1]);    // Read in command line argument (as int)
+    const int n = atoi(argv[1]);            // Read in command line argument (as int)
+
+    if (n < 1) {                            // Error if inputted n < 1
+        printf("n must be >= 1");
+        return 1;
+    }
+
     printf("%d", n);
 
     return 0;
