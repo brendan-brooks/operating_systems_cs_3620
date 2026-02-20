1fd88d3b2f0da3aa0d9dc6af256af8cfc314fabf
brenux <brendan-brooks@uiowa.edu>
2026-02-20 17:37:25 -0600

    update format to match problem 1

--- hw1/problem_2/primes.c
+++ hw1/problem_2/primes.c
@@ -3,10 +3,15 @@
 
 int main(int argc, char* argv[]) {
 
+    if (argc != 2) {                // Check number of command line arguments
+        printf("You must give ONE command line argument, an int n\n");
+        return 1;
+    }
+
     const int n = atoi(argv[1]);    // Read in command line argument (as int)
 
     if (n < 1) {                    // Error if inputted n < 1
-        printf("n must be an int >= 1");
+        printf("n must be an int >= 1\n");
         return 1;
     }
 
