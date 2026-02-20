95458be6eb395751f6c6fe0ac4120bf8200b4177
brenux <brendan-brooks@uiowa.edu>
2026-02-20 15:46:17 -0600

    read in command line argument

--- hw1/problem_1/fibonacci.c
+++ hw1/problem_1/fibonacci.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>     // atoi()
 
 int main(int argc, char* argv[]) {
 
-    printf( "Hello world!\n" );
+    const int n = atoi(argv[1]);    // Read in command line argument (as int)
+    printf("%d", n);
 
     return 0;
 }
